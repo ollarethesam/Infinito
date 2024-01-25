ac40a544175b7bdfa2346493a9d5599c63da6edf
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 12:52:47 +0100

    saving on vps

--- DataManager/templatetags/get_next_field.py
+++ DataManager/templatetags/get_next_field.py
@@ -3,12 +3,11 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_next_field(form_fields, current_field_name):
-    form_keys = list(form_fields.keys())
+def get_next_field(form, current_field):
+    form_keys = list(form.fields.items.keys())
     try:
-        current_index = form_keys.index(current_field_name)
+        current_index = form_keys.index(current_field.name)
         next_field_name = form_keys[current_index + 1]
-        return form_fields[next_field_name]
+        return form.fields.items[next_field_name]
     except (ValueError, IndexError):
-        print(next_field_name)
         return None
