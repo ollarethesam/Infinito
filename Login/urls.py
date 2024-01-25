f1bc929fe7b5066c136a799f5989cd174ac34185
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 19:04:22 +0100

    saving on vps

--- Login/urls.py
+++ Login/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('signup', views.signup, name='signup'),
     path('sfondo', views.bg, name="bg"),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
