a72ababba17eed307037e417886aa28e2acd3909
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 19:38:31 +0100

    saving on vps

--- Infinito/settings.py
+++ Infinito/settings.py
@@ -129,10 +129,6 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/DataManager'),
-    os.path.join(BASE_DIR, 'static/Login'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
