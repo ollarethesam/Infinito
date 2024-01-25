6f1394970310c4eaa27b3f67c34f4269016ffe9c
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 12:07:39 +0100

    saving on vps

--- DataManager/templatetags/ctt.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag
-def get_next_field(form, index):
-    return form[index + 1] if index + 1 < form|length else None
