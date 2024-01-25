7c91bc38f1e0d6d7cef5b37b275cc11afdf62596
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 13:32:54 +0100

    saving on vps

--- DataManager/templatetags/get_next_field.py
+++ DataManager/templatetags/get_next_field.py
@@ -8,6 +8,6 @@ def get_next_field(form, field):
         keys = list(form.fields.keys())
         index = keys.index(field.name)
         next_field_name = keys[index + 1]
-        return form.next_field_name
+        return form[next_field_name]
     except (ValueError, IndexError):
         return None
