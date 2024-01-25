2fa487bc5f1cc34f29de9b7bb755e036595ab15d
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 12:44:06 +0100

    saving on vps

--- DataManager/templatetags/get_next_field.py
+++ DataManager/templatetags/get_next_field.py
@@ -6,8 +6,8 @@ register = template.Library()
 def get_next_field(form, current_field):
     try:
         form_fields = list(form)
-        print(form_fields)
         current_index = form_fields.index(current_field)
+        print(current_index)
         return form[form_fields[current_index + 1]]
     except (ValueError, IndexError) as e:
         print(e)
