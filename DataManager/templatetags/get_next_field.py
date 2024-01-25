db40c2d32f5c333631021b6569b534216ec8bfad
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 12:37:10 +0100

    saving on vps

--- DataManager/templatetags/get_next_field.py
+++ DataManager/templatetags/get_next_field.py
@@ -3,8 +3,6 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_next_field(form, current_field):
-    form_fields = list(form)
-    current_index = form_fields.index(current_field)
-    print(current_index)
-    return current_index
+def get_next_field(form, field):
+    index = form[field]
+    return index
