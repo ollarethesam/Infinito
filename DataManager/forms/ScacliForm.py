674bf72a2ec170bb0be5c4811bd460b5b7602c54
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 10:57:51 +0100

    saving on vps

--- DataManager/forms/ScacliForm.py
+++ DataManager/forms/ScacliForm.py
@@ -5,7 +5,7 @@ from django.forms import ModelForm
 from DataManager.models.scacli import Scacli
 
 def get_last_numpro():
-        if Scacli.objects.last().exists():
+        if Scacli.objects.last():
             return Scacli.objects.last().pk
         else:
             return 1
