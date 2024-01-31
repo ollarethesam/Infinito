214369b5fec3ce98b56788c759970a70afa0c7ce
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 11:44:19 +0100

    saving on vps

--- DataManager/models/artico.py
+++ DataManager/models/artico.py
@@ -2,7 +2,7 @@ from django.db import models
 from Login.models import CustomUser
 
 class Artico(models.Model):
-    codart = models.CharField(primary_key=True, max_length=20)
+    codart = models.CharField(primary_key=True, max_length=16)
     desart = models.CharField(max_length=40)
     prezzo = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(CustomUser, to_field='username', on_delete=models.CASCADE)
