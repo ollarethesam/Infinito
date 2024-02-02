1d56c02c85eea91b344287a08cef4ad01aee7ab8
BuildTools <unconfigured@null.spigotmc.org>
2024-02-02 11:52:54 +0100

    saving on vps

--- DataManager/templatetags/get_next_field.py
+++ DataManager/templatetags/get_next_field.py
@@ -7,7 +7,13 @@ def get_next_field(form, field):
     try:
         keys = list(form.fields.keys())
         index = keys.index(field.name)
+        i = 1
         next_field_name = keys[index + 1]
-        return form[next_field_name]
+        no_label_fields = []
+        while not form[next_field_name].label:
+            no_label_fields.append(form[next_field_name])
+            next_field_name = keys[index + i]
+            i += 1
+        return no_label_fields
     except (ValueError, IndexError):
         return ''
